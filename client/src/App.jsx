4b280f7293464397bff8ecb7250d98250739a5e1
Vasjan Çupri <vasjancupri04@gmail.com>
2023-03-04 16:43:49 +0100

    added a client side and a simple form

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import InputTodo from "./components/InputTodo"
+function App() {
+
+  return (
+    <>
+    <div class="container">
+      <InputTodo />
+
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/InputTodo.jsx
@@ -0,0 +1,19 @@
+import {useState} from "react"
+
+export default function InputTodo() {
+    const [description, setDescription] = useState("")
+  return (
+    <>
+      <h1 className="text-center mt-5">Pern Todo List</h1>
+      <form className="d-flex mt-5">
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
+    </>
+  )
+}
